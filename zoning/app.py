3528a45257f7198fdf36c9593ac2ca1a6acc1336
Lukas <lukasboelling002@gmail.com>
2020-06-09 20:20:30 -0400

    Make app.py work with heroku
    Changed the host to 0.0.0.0 and port to the "HOST" environment variable

--- zoning/app.py
+++ zoning/app.py
@@ -1,5 +1,6 @@
 import json
 import glob 
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def load_maps():
 
     geojson = []
-    for filename in glob.iglob('geojson/*.json'):
+    for filename in glob.iglob('zoning/geojson/*.json'):
         with open(filename) as f:
             geojson.append(json.load(f))
 
@@ -17,4 +18,5 @@ def load_maps():
     return render_template('pymap.html', zones=geojson)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
