b569c2189b069d110d04dc01ae595c8939136da3
Lukas <lukasboelling002@gmail.com>
2020-06-11 21:51:28 -0400

    Add links from map zones to bylaw descriptions

--- zoning/app.py
+++ zoning/app.py
@@ -6,10 +6,10 @@ from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root='assets/')
+app.wsgi_app.add_files('assets/bylaws/', prefix='bylaw')
 
 @app.route("/")
 def load_maps():
-
     geojson = []
     for filename in glob.iglob('zoning/geojson/*.json'):
         with open(filename) as f:
@@ -17,9 +17,18 @@ def load_maps():
 
     #with open('templates/static.html', 'w') as f:
     #    f.write(render_template('pymap.html', zones=geojson))
-    return render_template('pymap.html', zones=geojson)
+    return render_template('map.html', zones=geojson)
+
+@app.route("/zone/<zoning_code>")
+def show_bylaws(zoning_code):
+    category = zoning_code.split('-')[0]
+    codes = zoning_code.split('-')[1:]
+    print(zoning_code)
+    image_links = [f"bylaw/{code}.png" for code in codes]
+
+    return render_template('bylaw_display.html', category=category, image_links=image_links)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     host = '0.0.0.0'
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
