a1040a751d58e2626b8cf65437bbd4b4d1a0db89
Lukas <lukasboelling002@gmail.com>
2020-06-19 02:43:10 -0400

    Change settings.py to use environment variables

--- zoningsite/zoningsite/settings.py
+++ zoningsite/zoningsite/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'opx*o!!xv9%*xr2#cnfaf60$_1&5lh1d(^5)$)(y*gjipfofzu'
+#SECRET_KEY = 'opx*o!!xv9%*xr2#cnfaf60$_1&5lh1d(^5)$)(y*gjipfofzu'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'opx*o!!xv9%*xr2#cnfaf60$_1&5lh1d(^5)$)(y*gjipfofzu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '.herokuapp.com']
 
 
 # Application definition
