8e903e42db55de410549d554140a01547a8a0f33
Lukas <lukasboelling002@gmail.com>
2020-06-19 02:44:04 -0400

    Add redirect from root url to map view

--- zoningsite/zoningsite/urls.py
+++ zoningsite/zoningsite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path('', lambda r: HttpResponseRedirect('map/')),
     path('map/', include('map.urls')),
     path('bylaw/', include('bylaw_view.urls')),
     path('admin/', admin.site.urls),
 ]
+
