13d7613739e76da46a4d767ec9e5e2f53ba03004
Lukas <lukasboelling002@gmail.com>
2020-06-21 14:27:10 -0400

    Fix syntax error in load_map.js

--- app/static/js/load_map.js
+++ app/static/js/load_map.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
     $.get('zones', function(data, status) {
         console.log(status);
         console.log(data);
-        var zones = data;
+        var specZones = data;
 
         load_map(zones);
     });
 });
 
-function load_map(zones) {
+function load_map(specificationZones, exceptionZones) {
     var mymap = L.map('mapid').setView([43.725, -79.232], 17);
     var mapboxUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
-    var attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+    var attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
     L.tileLayer(mapboxUrl, {
         attribution: attribution,
