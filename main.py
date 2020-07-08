a813f68655e3dc4bfc56b0fdb4a5cc7217386e59
Lukas <lukasboelling002@gmail.com>
2020-07-07 20:16:29 -0400

    Create main.py to generate static files

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from convert import read_dxf as rdxf
+from convert import read_docx as rdocx
 
 
 if __name__ == '__main__':
-    reader = rdxf.DxfReader("convert/ccrest_marked.dxf", 'cliffcrest', 'z_regions', 'z_standards', 'exc_regions', 'exc_standards')
-    reader.save_geojson('spec', 'app/static/geojson/specifications')
-    reader.save_geojson('exc', 'app/static/geojson/exceptions')
+    docx = rdocx.DocxBylawReader('convert/CCREST.docx', 'cliffcrest')
+    docx.save_bylaws('spec', 'app/static/bylaws/specifications')
+    docx.save_bylaws('exc', 'app/static/bylaws/exceptions')
+
+    dxf = rdxf.DxfReader("convert/ccrest_marked.dxf", 'cliffcrest', 'z_regions', 'z_standards', 'exc_regions', 'exc_standards')
+    dxf.save_geojson('spec', 'app/static/geojson/specifications')
+    dxf.save_geojson('exc', 'app/static/geojson/exceptions')
